168368325f74121c0d26869597f2e3cce9faa804
co253 <co253@cornell.edu>
2019-10-28 17:29:46 -0400

    stuff

---
+++ schedule.ml
@@ -0,0 +1,12 @@
+
+type course = {
+
+}
+
+type = semester {
+
+  }
+
+type schedule = {
+  semesters: 
+}
---
+++ schedule.mli
@@ -0,0 +1,9 @@
+
+(** Type representing a course *)
+type course
+
+(** Type representing a semester *)
+type semester
+
+(** Type representing a whole schedule *)
+type schedule
