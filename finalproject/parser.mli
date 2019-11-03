a59fc1524182d5adad7a2a473378b0940944ba95
co253 <co253@cornell.edu>
2019-11-02 20:38:43 -0400

    added parser

---
+++ finalproject/parser.mli
@@ -0,0 +1,6 @@
+open Schedule
+
+(** [get_course_info nm sem] is a [course] populated with info on 
+    course with name [nm] during semester [sem] gotten from Class
+    Roster website. *)
+val get_course_info : string -> string -> course
--- finalproject/schedule.ml
+++ finalproject/schedule.ml
@@ -1,5 +1,6 @@
 type sem_status = Past | Present | Future
 type grade = Sat | Unsat | Withdrawn | Incomplete | Letter of string 
+
 type reqs = {
   temp: int;
 }
@@ -13,15 +14,15 @@ type course = {
 }
 
 type semester = {
-  (*make mutable?*)courses: course list;
-  tot_credits: int;
+  mutable courses: course list;
+  mutable tot_credits: int;
   mutable sem_status: sem_status;
-  sem_gpa: float;
+  mutable sem_gpa: float;
 }
 
 type schedule = {
-  (*make mutable?*)semesters: semester list;
-  curr_gpa: float;
+  mutable semesters: semester list;
+  mutable curr_gpa: float;
   mutable exp_grad: int;
   mutable major: string;
 
