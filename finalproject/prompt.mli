492a5e7460313829f51074afbbd95a62b6e3e952
co253 <co253@cornell.edu>
2019-11-02 20:12:46 -0400

    stuff

---
+++ finalproject/prompt.mli
@@ -0,0 +1,4 @@
+(* Command Structure:
+    Prompt should ask to create new or open saved schedule
+    Then prompt commands for add / modify / remove courses or semesters 
+    from schedules... *)
--- prompt.mli
+++
@@ -1,4 +0,0 @@
-(* Command Structure:
-    Prompt should ask to create new or open saved schedule
-    Then prompt commands for add / modify / remove courses or semesters 
-    from schedules... *)
