3c94b5bf940957df0e9e47fa75d93a7ff0be4c75
ls769 <ls769@cornell.edu>
2019-11-05 23:46:08 -0500

    changing prompt only

--- finalproject/prompt.ml
+++ finalproject/prompt.ml
@@ -15,25 +15,44 @@ exception Empty
 
 exception Malformed
 
-let edit_others str_lst =
+(** [sem_id_parse sem_id] parses [sem_id] if is a valid semester type.
+    Raises: Malformed when [sem_id] is not valid. *)
+let sem_id_parse sem_id =
+  if Str.string_match (Str.regexp "^SP[0-9]{2}$") sem_id 0 
+  then Spring (int_of_string (String.sub sem_id 2 2))
+  else if Str.string_match (Str.regexp "^FA[0-9]{2}$") sem_id 0 
+  then Fall (int_of_string (String.sub sem_id 2 2)) else raise Malformed
+
+let add_others sch str_lst =
   match str_lst with
-  | class_name::field::[] when  
+  | [] -> raise Empty
+  | course_name::grade::degree::sem_id::[] -> 
+    add_course sch (create_course course_name (get_course_info course_name sem_id) grade degree) (sem_id_parse sem_id)
+  | course_name::credits::grade::degree::sem_id::[] -> 
+    add_course sch (create_course course_name (int_of_string credits) grade degree) sem_id
+  | _ -> raise Malformed
 
-      let parse_command cmd_str = 
+let edit_others str_lst sch =
+  match str_lst with
+  | [] -> raise Empty
+  | course_name::field::[] -> edit_course sch course_name field
+  | _ -> raise Malformed 
 
-        let first_match_helper first others =
-          match first with
-          | "add" -> Add others
-          | "edit" -> Edit edit_others (others)
-          | "remove" -> Remove others
-          | _ -> raise Malformed
-        in
+let parse_command cmd_str sch = 
 
-        let split_cmd = String.split_on_char ' ' cmd_str in
-        match (split_cmd) with 
-        | [] -> raise Empty
-        | fst::[] -> raise Malformed
-        | fst::others -> match_helper fst others
+  let first_match_helper first others =
+    match first with
+    | "add" -> Add add_others (others sch)
+    | "edit" -> Edit edit_others (others sch)
+    | "remove" -> Remove others
+    | _ -> raise Malformed
+  in
+
+  let split_cmd = String.split_on_char ' ' cmd_str in
+  match (split_cmd) with 
+  | [] -> raise Empty
+  | fst::[] -> raise Malformed
+  | fst::others -> match_helper fst others
 
 
 
