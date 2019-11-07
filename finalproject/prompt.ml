5f145e8232ffc6ec71d4dd0bff20bc162b6bbcfc
ls769 <ls769@cornell.edu>
2019-11-06 21:16:50 -0500

    nothing

--- finalproject/prompt.ml
+++ finalproject/prompt.ml
@@ -12,9 +12,10 @@ exception MalformedRemove
 (** [sem_id_parse sem_id] parses [sem_id] if is a valid semester type.
     Raises: Malformed when [sem_id] is not valid. *)
 let sem_id_parse sem_id =
-  if Str.string_match (Str.regexp "^SP[0-9][0-9]$") sem_id 0 then
+  let uppercase_id = String.uppercase_ascii sem_id in
+  if Str.string_match (Str.regexp "^SP[0-9][0-9]$") uppercase_id 0 then
     Spring (int_of_string (String.sub sem_id 2 2))
-  else if Str.string_match (Str.regexp "^FA[0-9][0-9]$") sem_id 0 then 
+  else if Str.string_match (Str.regexp "^FA[0-9][0-9]$") uppercase_id 0 then 
     Fall (int_of_string (String.sub sem_id 2 2)) 
   else 
     raise MalformedSemId
@@ -24,12 +25,15 @@ let add_others sch str_lst =
   match str_lst with
   | [] -> raise MalformedAdd
   | "sem"::sem_id::[] ->
-    add_sem sch (create_sem (sem_id_parse (String.capitalize_ascii sem_id)))
+    add_sem sch (create_sem (sem_id_parse sem_id))
   | course_name::grade::degree::sem_id::[] -> 
-    add_course sch 
-      (create_course course_name (get_course_creds course_name (sem_id_parse sem_id)) (Schedule.gradify grade) degree) (sem_id_parse sem_id)
+    let curr_credits = get_course_creds course_name (sem_id_parse sem_id) in
+    let curr_course = create_course course_name curr_credits (gradify grade) degree in
+    add_course sch curr_course (sem_id_parse sem_id)
   | course_name::credits::grade::degree::sem_id::[] -> 
-    add_course sch (create_course course_name (int_of_string credits) (Schedule.gradify grade) degree) (sem_id_parse sem_id)
+    let curr_credits = int_of_string credits in
+    let curr_course = create_course course_name curr_credits (gradify grade) degree in
+    add_course sch curr_course (sem_id_parse sem_id)
   | _ -> sch
 
 (** [edit_others sch str_lst] parses [str_lst] in [sch] for the Edit command. *)
@@ -45,7 +49,7 @@ let remove_others sch str_lst =
   match str_lst with
   | [] -> raise MalformedRemove
   | "sem"::sem_id::[] -> 
-    remove_sem sch (sem_id_parse (String.uppercase_ascii sem_id))
+    remove_sem sch (sem_id_parse sem_id)
   | course_name::[] -> remove_course sch course_name
   | _ -> raise Malformed
 
@@ -64,6 +68,3 @@ let parse_command sch cmd_str =
   | [] -> raise Empty
   | "print"::[] -> (print_schedule sch); sch
   | fst::others -> match_helper fst others
-
-
-
