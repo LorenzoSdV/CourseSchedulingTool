56475a49a7fef6f74de5bd0ebc32102357f8c07e
ls769 <ls769@cornell.edu>
2019-11-21 00:46:01 -0500

    sys_error

--- finalproject/command.ml
+++ finalproject/command.ml
@@ -74,7 +74,8 @@ let remove_others sch str_lst =
 
 let export_handler sch str_lst = 
   match str_lst with
-  | file :: [] -> HTML.export_schedule sch file; sch
+  | file :: [] -> if Yojson.Basic.from_file file = Sys_error 
+    then (HTML.export_schedule sch file; sch) else raise MalformedExport
   | _ -> raise MalformedExport
 
 let save_handler sch str_lst = 
